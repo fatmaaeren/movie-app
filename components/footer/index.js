6ff53d7dd3ffb95a77a50ccff4ccded96b6bb428
fatmaaeren <124514606+fatmaaeren@users.noreply.github.com>
2023-05-22 20:49:54 +0300

    first

---
+++ components/footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./styles.module.css";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      Made with ❤️
+    </footer>
+  );
+}
+
+export default Footer;
